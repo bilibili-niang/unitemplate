df9e2da16aca3719a96b3d2b602cf4fabe16c993
张嘉凯 <killicestone@126.com>
2025-05-04 17:19:12 +0800

    feat(components): 添加 tabBars 组件并更新首页使用
    - 新增 tabBars 组件，用于显示标签栏
    - 在首页引入并使用 tabBars 组件
    - 添加 Tabbars 数据，包含首页、购物车和我的三个标签

---
+++ src/components/tabBars/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tab-bars">
+
+    <div class="tab-item" v-for="item in tabs">
+
+      {{ item }}
+
+    </div>
+
+  </div>
+</template>
+<script language="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  tabs: {
+    type: Array,
+    default: () => {
+      return []
+    },
+  },
+  active: {
+    type: Number,
+    default: 0,
+  },
+})
+
+</script>
+<style lang="scss">
+.tab-bars {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -13,13 +13,16 @@
       :defaultDate="defaultDate"
       @submit="getSubmitData"
     />
-
+    <TabBars
+      :tabs="Tabbars"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import datePicker from '../../components/thirdComponents/nj-birthday-picker/nj-birthday-picker.vue'
+import TabBars from '../../components/tabBars/index.vue'
 
 const pickerRef = ref({
   clone: () => 0,
@@ -55,6 +58,25 @@ const showDate = () => {
     return '请选择日期'
   }
 }
+const avtive=ref('0')
+
+const Tabbars = ref([
+  {
+    text: '首页',
+    icon: 'icon-shouye',
+    active: '0',
+  },
+  {
+    text: '购物车',
+    icon: 'icon-gouwuche',
+    active: '1',
+  },
+  {
+    text: '我的',
+    icon: 'icon-wode',
+    active: '2',
+  },
+])
 
 </script>
 
