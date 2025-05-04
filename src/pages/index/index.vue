026b81f51b21d7656ed10a6c797455b5af1602ee
张嘉凯 <killicestone@126.com>
2025-05-04 16:44:29 +0800

    feat(components): 添加生日选择器组件
    - 新增 nj-birthday-picker 组件，支持公历和农历日期选择
    - 添加 vue3-hash-calendar 依赖用于显示日历- 引入 js-calendar-converter 用于公农历转换
    - 在页面中集成新组件并添加测试用例

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div
-    class="content">
+    class="content"
+    @click="test"
+  >
 
     <uni-datetime-picker
       ref="picker"
@@ -12,20 +14,40 @@
     >
     </uni-datetime-picker>
 
+    <vue-hash-calendar>
+      <template #action>
+        <div class="">我是自定义的操作栏</div>
+      </template>
+    </vue-hash-calendar>
+
+    <datePicker
+      ref="pickerRef"
+      :defaultLunar="true"
+      :defaultDate="defaultDate"
+    />
 
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import VueHashCalendar from 'vue3-hash-calendar'
+import 'vue3-hash-calendar/es/index.css'
+import datePicker from '../../components/thirdComponents/nj-birthday-picker/nj-birthday-picker.vue'
 
-
+const pickerRef = ref(null)
 const picker = ref(null)
+
+const defaultDate = new Date()
 
 const single = '2021-04-3'
 
 const change = (e: string) => {
   console.log('-change事件:', e)
+}
+const test = () => {
+  console.log('pickerRef', pickerRef.value)
+  pickerRef.value?.show()
 }
 </script>
 
