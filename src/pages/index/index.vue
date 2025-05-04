428ab96915559b2d7851df84f75686f35411ca50
张嘉凯 <killicestone@126.com>
2025-05-04 16:58:36 +0800

    feat(index): 重构首页日期选择功能
    - 移除了 uni-datetime-picker 和 vue-hash-calendar 组件
    - 添加了自定义的 datePicker 组件
    - 实现了显示和选择日期的功能
    - 优化了代码结构，提高了代码的可读性和维护性

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,26 +4,14 @@
     @click="test"
   >
 
-    <uni-datetime-picker
-      ref="picker"
-      type="date"
-      :value="single"
-      start="1901-1-01"
-      end="2500-5-30"
-      @change="change"
-    >
-    </uni-datetime-picker>
-
-    <vue-hash-calendar>
-      <template #action>
-        <div class="">我是自定义的操作栏</div>
-      </template>
-    </vue-hash-calendar>
-
+    <div @click="showPicker">
+      {{ showDate() }}
+    </div>
     <datePicker
       ref="pickerRef"
       :defaultLunar="true"
       :defaultDate="defaultDate"
+      @submit="getSubmitData"
     />
 
   </div>
@@ -31,24 +19,43 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import VueHashCalendar from 'vue3-hash-calendar'
-import 'vue3-hash-calendar/es/index.css'
 import datePicker from '../../components/thirdComponents/nj-birthday-picker/nj-birthday-picker.vue'
 
-const pickerRef = ref(null)
-const picker = ref(null)
+const pickerRef = ref({
+  clone: () => 0,
+  show: () => 0,
+})
 
-const defaultDate = new Date()
+const defaultDate = ref(new Date())
+const date = ref<any>({})
 
-const single = '2021-04-3'
-
-const change = (e: string) => {
-  console.log('-change事件:', e)
-}
 const test = () => {
-  console.log('pickerRef', pickerRef.value)
+  // console.log('pickerRef', pickerRef.value)
+  console.log('ref:')
+  console.log(pickerRef.value)
+}
+
+const showPicker = () => {
   pickerRef.value?.show()
 }
+
+const getSubmitData = (e: any) => {
+  date.value = e
+}
+const showDate = () => {
+  if (date.value?.cYear) {
+    if (date.value.isLunar) {
+      // 阴历
+      return `${date.value.cYear}(${date.value.gzYear}) - ${date.value.cMonth}(${date.value.gzMonth}) - ${date.value.cDay}(${date.value.gzDay}) `
+    } else {
+      // 阳历
+      return `${date.value.cYear} - ${date.value.cMonth} - ${date.value.cDay} `
+    }
+  } else {
+    return '请选择日期'
+  }
+}
+
 </script>
 
 <style></style>
