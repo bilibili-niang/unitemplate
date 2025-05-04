26191d713ab28884aff5d0021c8166c714392458
张嘉凯 <killicestone@126.com>
2025-05-04 16:26:21 +0800

    refactor(index): 重构首页组件
    - 移除了原有的模板结构和样式
    - 添加了 uni-datetime-picker 组件用于日期选择- 更新了 script 部分的逻辑，增加了日期选择相关的变量和方法
    - 在 package.json 中添加了 uni-ui 依赖并更新了相关版本
    - 添加了 sass 和 sass-loader 依赖用于样式处理

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,95 +1,32 @@
-<!-- <template>  
-  <view class="content">  
-    <image class="logo" src="/static/logo.png" />  
-    <view class="text-area">  
-      <text class="title">{{ title }}</text>  
-    </view>  
-    <view>会员ID: {{ profileId }}</view>  
-    <view>会员姓名: {{ profileName }}</view>  
-    <view>是否会员：{{ isMember ? '是' : '否' }}</view>  
-    <button @click="updateName">修改名字</button>  
-    <br />  
-    <view>以下是应用名称：</view>  
-    <view style="font-weight: bold">{{ appName }}</view>  
-  </view>  
-</template>  
-  
-<script setup lang="ts">  
-import { useAppStore, useMemberStore } from '@/store';  
-  
-const memberStore = useMemberStore();  
-const appStore = useAppStore();  
-  
-// 使用计算属性来访问响应式数据  
-const profileId = computed(() => memberStore.profile.id);  
-const profileName = computed(() => memberStore.profile.name);  
-const isMember = computed(() => memberStore.profile.isMember);  
-const appName = computed(() => appStore.app.name);  
-  
-const title = 'Hello'; // 如果不需要响应式，可以直接使用字符串  
-  
-// 更新名字的方法  
-const updateName = () => {  
-  memberStore.setProfile({ ...memberStore.profile, name: '李四', isMember: false });  
-};  
-</script>   -->
-
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <view>{{ memberStore.profile.id }}</view>
-    <view>{{ profileId }}</view>
-    <view>是否会员：{{ memberStore.profile.isMember ? '是' : '否' }}</view>
-    <view>{{ profileName }}</view>
-    <button @click="updateName">修改名字</button>
-    <br />
-    <view>以下是应用名称：</view>
-    <view style="font-weight: bold">{{ appStore.app.name }}</view>
-  </view>
+  <div
+    class="content">
+
+    <uni-datetime-picker
+      ref="picker"
+      type="date"
+      :value="single"
+      start="1901-1-01"
+      end="2500-5-30"
+      @change="change"
+    >
+    </uni-datetime-picker>
+
+
+  </div>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue'
-import { useAppStore, useMemberStore } from '@/store'
+import { ref } from 'vue'
 
-const memberStore = useMemberStore()
-const appStore = useAppStore()
-const profileId = computed(() => memberStore.profile.id)
 
-const profileName = computed(() => memberStore.profile.name)
-const title = ref('Hello')
-const updateName = () => {
-  memberStore.setProfile({ ...memberStore.profile, name: '李四', isMember: false })
+const picker = ref(null)
+
+const single = '2021-04-3'
+
+const change = (e: string) => {
+  console.log('-change事件:', e)
 }
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
